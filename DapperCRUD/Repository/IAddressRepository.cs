7b4b9bfec29b5a3443a4a1191a739ef5ba9d2521
tayeb <tayeb.mora69@gmail.com>
2023-08-16 16:46:03 +0330

    Add IAddressRepository

---
+++ DapperCRUD/Repository/IAddressRepository.cs
@@ -0,0 +1,12 @@
+﻿using DapperCRUD.Models;
+
+namespace DapperCRUD.Repository
+{
+    public interface IAddressRepository
+    {
+        Task<AddressDto> GetByAddres(int id);
+        Task Create(Customer _Customer);
+        Task Update(Customer _Customer);
+        Task Delete(int id);
+    }
+}
