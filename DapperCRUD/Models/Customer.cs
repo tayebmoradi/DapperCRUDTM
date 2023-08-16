c9ef42d18161283ee0598b494dedd27409099cf3
tayeb <tayeb.mora69@gmail.com>
2023-08-16 10:57:41 +0330

    add CustomerDto

--- DapperCRUD/Models/Customer.cs
+++ DapperCRUD/Models/Customer.cs
@@ -10,4 +10,14 @@
 
         public List<Address> Addresses { get; set; }
     }
+    public class CustomerDto : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public string NationalCode { get; set; }
+
+        public List<Address> Addresses { get; set; }
+    }
 }
